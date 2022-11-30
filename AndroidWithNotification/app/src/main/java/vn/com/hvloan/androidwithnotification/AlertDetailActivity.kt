8150f1a311ab64d7d864e2424199a631bfc05d1d
hvloan <hvloan.work@gmail.com>
2022-11-30 14:07:30 +0700

    Update android with notification app

---
+++ AndroidWithNotification/app/src/main/java/vn/com/hvloan/androidwithnotification/AlertDetailActivity.kt
@@ -0,0 +1,11 @@
+package vn.com.hvloan.androidwithnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlertDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alert_detail)
+    }
+}
